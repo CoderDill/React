8a3622ad9b002b85c8a1347efa9d007053cb8b13
CrappyDill <mddillon47@gmail.com>
2021-06-30 14:33:04 -0700

    Lights Out done

---
+++ lights-out/src/App.js
@@ -0,0 +1,13 @@
+import Board from "./Board";
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <h1 className="App-title">Lights Out</h1>
+      <Board />
+    </div>
+  );
+}
+
+export default App;
