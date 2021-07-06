1b7de316bae45004c445d148776612c6e22f046e
CrappyDill <mddillon47@gmail.com>
2021-07-06 15:23:15 -0700

    Timer/Counter

---
+++ use-effect/src/Counter.js
@@ -0,0 +1,22 @@
+import { useState, useEffect } from "react";
+
+const Counter = () => {
+  const [num, setNum] = useState(0);
+
+  const increment = () => {
+    setNum((n) => n + 1);
+  };
+
+  useEffect(() => {
+    document.title = `Hi${"!".repeat(num)}`;
+  }, []);
+  return (
+    <>
+      Get excited!
+      <button onClick={increment}>Get More Excited!</button>
+      <p>{num}</p>
+    </>
+  );
+};
+
+export default Counter;
